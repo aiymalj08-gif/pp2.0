2d80c78356366c3e3dc20762103a616bf2a1f7be
aiymalj08-gif <aiymalzhanbekova08@gmail.com>
2026-02-01 21:20:16 +0500

    Add Practice2 - Python control flow examples (boolean)

--- Practice2/boolean/bool_intro.py
+++ Practice2/boolean/bool_intro.py
@@ -1,3 +1,15 @@
-print(10 > 9)
-print(10 == 9)
-print(10 < 9)
+is_student = True
+has_homework = False
+
+print("Is student:", is_student) 
+print("Has homework:", has_homework)
+
+age = 17
+is_adult = age >= 18 # condition that compares age variable and returns False
+
+print("Age:", age)
+print("Is adult:", is_adult) # Here returns
+
+# Boolean from expression
+result = (10 > 5)
+print("Is 10 greater than 5?", result)
