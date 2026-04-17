58b96eef0be247b7815774369a3980a5a9449fa7
aiymalj08-gif <aiymalzhanbekova08@gmail.com>
2026-04-17 17:07:44 +0500

    save before pull

--- Practice7/connect.py
+++ Practice7/connect.py
@@ -2,4 +2,4 @@ import psycopg2
 from config import DB_CONFIG
 
 def connect():
-    return psycopg2.connect(**DB_CONFIG)
+    return psycopg2.connect(**DB_CONFIG)#connects the created DB dictionary and connects it with psycopg2
