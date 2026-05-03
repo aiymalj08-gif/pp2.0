ce84e6f89359e5c70c6230250698befe0b5aab27
aiymalj08-gif <aiymalzhanbekova08@gmail.com>
2026-05-03 21:05:07 +0500

    TSIS4 Snake

--- TSIS4/db.py
+++ TSIS4/db.py
@@ -20,7 +20,7 @@ from psycopg2 import sql
 DB_CONFIG = {
     "dbname":   "snake_db",
     "user":     "postgres",
-    "password": "postgres",
+    "password": "1234",
     "host":     "localhost",
     "port":     5432,
 }
